149701ee91b7411260166e86e11cadd7885f471a
Agnieszka Brown <abrown2012@icloud.com>
2021-05-24 16:53:27 -0400

    commit tree ed72693acab351b33785306e992232ce85b7891e

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
   //add static methods here
+  static capitalize(someString){
+    return someString.charAt(0).toUpperCase() + someString.slice(1)
+  }
+
+  static sanitize(someString) {
+    return someString.replace(/[^0-9a-z\s:\.'\.-]/gi, '')
+  }
+
+  static titleize(someString) {
+    let newSomeString = someString.split(" ")
+    let myArray = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+    let finalString = []
+    let Blas = newSomeString.map(word => {
+      if (!myArray.includes(word) || word === newSomeString[0]){
+        return word.charAt(0).toUpperCase() + word.slice(1)
+        
+      } else {
+        return word
+      }
+    
+    })
+    return Blas.join(' ')
+  }
 }
